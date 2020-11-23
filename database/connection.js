e0152775b1f34d561b525fcb464718ff8a7f2a68
PaolaRodriguesLopes <paolarodrigues158@gmail.com>
2020-11-23 13:17:39 -0300

    backend

--- database/connection.js
+++ database/connection.js
@@ -1,27 +1,27 @@
-// var knex = require('knex')({
-//     client: 'mysql2',
-//     connection: {
-//       host : 'weknow.cqeueigzxw1e.us-east-2.rds.amazonaws.com',
-//       user : 'admin',
-//       password: 'Facens2020',
-//       database : 'weknow'
-//     }
-//   });
+ var knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+      host : 'weknow.cqeueigzxw1e.us-east-2.rds.amazonaws.com',
+      user : 'admin',
+      password: 'Facens2020',
+      database : 'weknow'
+    }   
+  });
 
 
 
-var knex = require('knex')({
-  client: 'mysql2',
-  connection: {
-    host: '127.0.0.1',
+//var knex = require('knex')({
+  //client: 'mysql2',
+  //connection: {
+    //host: '127.0.0.1',
     //host: 'localhost',
-    user: 'root',
-    password: 'root',
+    //user: 'root',
+    //password: 'root',
     // password: '1234',
     //password: '',
-    database: 'weknow'
-  }
-});
+    //database: 'weknow'
+  //}
+//});
 
 
 module.exports = knex
